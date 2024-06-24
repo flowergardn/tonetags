66e2d851b8e825c51ef90119665b291f99535863
astridlol <me@astrid.sh>
2024-06-24 10:44:42 -0400

    feat: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), mdx()]
+  }), mdx()],
+  adapter: vercel()
 });
